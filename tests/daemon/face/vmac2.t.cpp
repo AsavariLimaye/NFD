039cda7e508d604d193919015c0e8839353f3a3c
Asavari Limaye <asavari2@g.ucla.edu>
2019-12-06 22:33:59 -0800

    Change mtu

--- tests/daemon/face/vmac2.t.cpp
+++ tests/daemon/face/vmac2.t.cpp
@@ -59,7 +59,7 @@ protected:
 
   void
   initialize(const VmacLinkService::Options& options,
-             ssize_t mtu = 2000,
+             ssize_t mtu = 30,
              ssize_t sendQueueCapacity = QUEUE_UNSUPPORTED)
   {
     face = make_unique<Face>(make_unique<VmacLinkService>(options),
@@ -112,10 +112,10 @@ BOOST_AUTO_TEST_CASE(SendInterest)
   
   initialize(options);
 
-  char interestName[3001] = "/localhost/test/";
-  for (int i=16; i<3000; i++)
+  char interestName[51] = "/localhost/test/";
+  for (int i=16; i<50; i++)
 	  interestName[i] = 'a';
-  interestName[3000] = '\0';
+  interestName[50] = '\0';
   printf("%d\n", strlen(interestName));
   auto interest1 =
   makeInterest(interestName);
